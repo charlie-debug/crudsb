772631b915027227533132b5b77b0d7be00afe40
charlie rodrigues <sistemajavaireport@gmail.com>
2025-12-23 11:12:44 -0300

    projeto criado

---
+++ src/main/java/com/charlierodrigues/backendsb/BackendsbApplication.java
@@ -0,0 +1,13 @@
+package com.charlierodrigues.backendsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charlierodrigues/backendsb/BackendsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlierodrigues.backendsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
