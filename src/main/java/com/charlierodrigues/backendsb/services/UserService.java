66d2c8d4492a66e6b8b514897bc91631aea2f1d8
charlie rodrigues <sistemajavaireport@gmail.com>
2025-12-26 20:29:50 -0300

    insercao de usuario com o POST

--- src/main/java/com/charlierodrigues/backendsb/services/UserService.java
+++ src/main/java/com/charlierodrigues/backendsb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.charlierodrigues.backendsb.domain.User;
+import com.charlierodrigues.backendsb.dto.UserDTO;
 import com.charlierodrigues.backendsb.repository.UserRepository;
 import com.charlierodrigues.backendsb.services.exception.ObjectNotFoundException;
 
@@ -15,14 +16,22 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
-			
+
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Usuário não encontrado!"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
