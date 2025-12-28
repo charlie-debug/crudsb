1b10d400b169e49fd08504ecefc081c4ed462543
charlie rodrigues <sistemajavaireport@gmail.com>
2025-12-28 20:07:54 -0300

    endpoint para retornar os posts dos usuarios

--- src/main/java/com/charlierodrigues/backendsb/config/Instantiation.java
+++ src/main/java/com/charlierodrigues/backendsb/config/Instantiation.java
@@ -35,7 +35,7 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("28/12/2025"), "partiu viagem", "Vou para Nova Zelândia!",new AuthorDTO(charlie));
 		Post post2 = new Post(null, sdf.parse("25/12/2025"), "Bom dia!", "Hoje é dia de fazer caminhada...",new AuthorDTO(giovana));
 		postRepository.saveAll(Arrays.asList(post1,post2));
-	   
+		
 		charlie.getPosts().add(post1);
 		giovana.getPosts().add(post2);
 		userRepository.saveAll(Arrays.asList(charlie,giovana));
