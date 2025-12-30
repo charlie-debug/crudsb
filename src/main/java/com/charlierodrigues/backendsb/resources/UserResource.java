92c72e1d3fdb74be8d9db5e96b4a861759fd1294
charlie rodrigues <sistemajavaireport@gmail.com>
2025-12-30 10:43:12 -0300

    encontrando um post por id

--- src/main/java/com/charlierodrigues/backendsb/resources/UserResource.java
+++ src/main/java/com/charlierodrigues/backendsb/resources/UserResource.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.charlierodrigues.backendsb.domain.Post;
 import com.charlierodrigues.backendsb.domain.User;
 import com.charlierodrigues.backendsb.dto.UserDTO;
@@ -28,41 +27,45 @@ public class UserResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-	    List<UserDTO> listDTO = list.stream().map(x-> new UserDTO(x)).collect(Collectors.toList());
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable  String id){
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
-		
+
 	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
 		User obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
+
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void>delete(@PathVariable String id){
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
+
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void>update(@RequestBody UserDTO userDTO, @PathVariable String id){
-     User obj = service.fromDTO(userDTO);
-     obj.setId(id);
-     obj = service.update(obj);
-     return ResponseEntity.noContent().build();
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User obj = service.fromDTO(userDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
+
 	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj.getPosts());
-		
 	}
 
 }
---
+++ src/main/java/com/charlierodrigues/backendsb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.charlierodrigues.backendsb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.charlierodrigues.backendsb.domain.Post;
+import com.charlierodrigues.backendsb.repository.PostRepository;
+import com.charlierodrigues.backendsb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(()-> new ObjectNotFoundException("post não encontrado!"));
+	}
+}
